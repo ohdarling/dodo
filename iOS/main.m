c32a22cc3dccbd90bfcdd6c4310ff7fa7a305f80
ohdarling88 <ohdarling88@gmail.com>
2011-10-16 19:05:21 +0800

    Add iOS target

---
+++ iOS/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  dodo
+//
+//  Created by Xu Jiwei on 11-10-5.
+//  Copyright Taobao.com 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
